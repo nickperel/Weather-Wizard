39938e355e0a4efb4e34c4ac3daaeec2d24fe7e6
Kakashi <nickperel@live.com>
2022-02-14 19:16:15 -0500

    set up weatherDash function which will house functionality for the whole website

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 const apiKey = "4e8ea4c48fc1caa38029a76e8f4dcc2b";
 
+function weatherDash() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let pastSearches = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(pastSearches);
 
 function getWeather(cityName) {
 
@@ -15,5 +28,8 @@ function getWeather(cityName) {
             console.log(data[i].local_names.ja);
         }
     });
-}
+  };
 
+};
+
+weatherDash();
